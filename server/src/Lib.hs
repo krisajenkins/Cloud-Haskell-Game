a3eff827c2e0a17e98dbc69b20ff923b4dc7ee6f
Kris Jenkins <krisajenkins@gmail.com>
2016-12-08 22:40:04 +0000

    Tidying and adding notes.

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -7,74 +7,28 @@
 module Lib where
 
 import           Control.Distributed.Process
-import           Control.Distributed.Process.Closure
 import           Control.Distributed.Process.Node
 import           Control.Distributed.Process.Serializable
 import           Control.Monad
 import           Control.Monad.Logger
 import           Control.Monad.Trans.Resource
 import           Data.Binary
+import           Data.Foldable
 import           Data.Monoid
 import qualified Data.Set                                 as Set
 import           Data.Text                                (Text)
 import qualified Data.Text                                as T
 import qualified Data.Text.Lazy                           as LT
 import qualified Data.Text.Lazy.Encoding                  as LTE
-import           Data.Typeable
 import           GHC.Generics
 import           Network.Transport.InMemory
 import qualified Network.WebSockets                       as WS
 
-data PlayerMessage =
-  Hello Int
-  deriving (Show, Eq, Generic, Binary, Typeable)
-
-instance Serializable PlayerMessage
-
+-- TODO All the channel/process names are horrible.
 -- TODO Disconnections don't detatch from the broadcaster, which sounds wrong.
+-- TODO Could really do with some monitoring.
 
 ------------------------------------------------------------
-data Echo =
-  Echo String
-  deriving (Show, Binary, Generic)
-
-echoProcess :: ReceivePort Echo -> Process ()
-echoProcess receiver = do
-  liftIO $ putStrLn "ECHO SERVER STARTING"
-  self <- getSelfPid -- get our own process id
-  liftIO . putStrLn $ "ECHO SERVER IS: " <> show self
-  forever $
-    do msg <- receiveChan receiver
-       liftIO . putStrLn $ "ECHO Process Heard: " <> show msg
-
-------------------------------------------------------------
-remotable []
-
-myRemoteTable :: RemoteTable
-myRemoteTable = __remoteTable initRemoteTable
-
-------------------------------------------------------------
-acceptClientConnection
-  :: MonadResource m
-  => LocalNode
-  -> SendPort Echo
-  -> SendPort GameMsg
-  -> SendPort (SendPort GameState)
-  -> WS.PendingConnection
-  -> m ()
-acceptClientConnection node echoProcessChannel txGameMsg txSubscribe pendingConnection = do
-  (_releaseKey, connection) <-
-    allocate
-      (runStdoutLoggingT $
-       do logInfoN "New connection received."
-          liftIO $ WS.acceptRequest pendingConnection)
-      (\_ -> runStdoutLoggingT $ logInfoN "Leaves")
-  void . liftIO . runProcess node $
-    do _ <- spawnLocal $ playerMsgProcess echoProcessChannel txGameMsg connection
-       (sendToMe, receiveFromBroadcaster) <- newChan
-       sendChan txSubscribe sendToMe
-       announceProcess connection receiveFromBroadcaster
-
 runGame :: IO ()
 runGame =
   let host = "127.0.0.1"
@@ -88,47 +42,52 @@ runGame =
         logInfoN "Forking listener."
         _ <-
           liftIO . runProcess node $
-          do (sendToEcho, receiveFromEcho) <- newChan
-             (sendSubGameState, receiveSubGameState) <- newChan
-             (txGameState :: SendPort GameState, rxGameState :: ReceivePort GameState) <-
-               newChan
+          do (sendSubGameState, receiveSubGameState) <- newChan
+             (txGameState, rxGameState) <- newChan
              (sendGameMsg, receiveGameMsg) <- newChan
-             _ <- spawnLocal $ echoProcess receiveFromEcho
              _ <- spawnLocal $ broadcastProcess rxGameState receiveSubGameState
-             _ <-
-               spawnLocal $
-               gameProcess
-                 receiveGameMsg
-                 txGameState
-                 GameState
-                 { lastMsg = Nothing
-                 , msgCount = 0
-                 }
+             _ <- spawnLocal $ gameProcess receiveGameMsg txGameState initialGameState
              liftIO . WS.runServer host websocketPort $
-               runResourceT .
-               acceptClientConnection node sendToEcho sendGameMsg sendSubGameState
+               runResourceT . acceptClientConnection node sendGameMsg sendSubGameState
         logInfoN "END"
 
-playerMsgProcess :: SendPort Echo
-                 -> SendPort GameMsg
-                 -> WS.Connection
-                 -> Process ()
-playerMsgProcess echoProcessPort txGameMsg connection = do
+acceptClientConnection
+  :: MonadResource m
+  => LocalNode
+  -> SendPort GameMsg
+  -> SendPort (SendPort GameState)
+  -> WS.PendingConnection
+  -> m ()
+acceptClientConnection node txGameMsg txSubscribe pendingConnection = do
+  (_releaseKey, connection) <-
+    allocate
+      (runStdoutLoggingT $
+       do logInfoN "New connection received."
+          liftIO $ WS.acceptRequest pendingConnection)
+      (\_ -> runStdoutLoggingT $ logInfoN "Leaves")
+  liftIO . runProcess node $
+    do _ <- spawnLocal $ receiveFromPlayerProcess txGameMsg connection
+       (sendToMe, receiveFromBroadcaster) <- newChan
+       sendChan txSubscribe sendToMe
+       announceToPlayerProcess connection receiveFromBroadcaster
+
+-- TODO Here we will decode GameMsgs from the JSON.
+receiveFromPlayerProcess :: SendPort GameMsg -> WS.Connection -> Process ()
+receiveFromPlayerProcess txGameMsg connection = do
   liftIO $ putStrLn "LISTENING"
-  sendChan echoProcessPort (Echo "Hello Echo Service I Am A Client")
   forever $
     do raw <- liftIO $ WS.receiveDataMessage connection
        case raw of
+         WS.Binary _ -> return ()
          WS.Text text -> do
            liftIO . putStrLn $ "HEARD: " <> show text
-           let echo = Echo (show text)
-           liftIO . putStrLn $ "SENDING TO ECHO: " <> show echo
-           sendChan echoProcessPort echo
            sendChan txGameMsg $ GameMsg (LT.toStrict (LTE.decodeUtf8 text))
-         WS.Binary _ -> return ()
 
-announceProcess :: WS.Connection -> ReceivePort GameState -> Process ()
-announceProcess connection rx =
+-- TODO Here we will encode the GameState to JSON.
+announceToPlayerProcess
+  :: (Show a, Serializable a)
+  => WS.Connection -> ReceivePort a -> Process b
+announceToPlayerProcess connection rx =
   forever $
   do msg <- receiveChan rx
      liftIO $ WS.sendTextData connection (T.pack (show msg))
@@ -138,9 +97,9 @@ data BroadcasterMsg a
   | Subscribe (SendPort a)
   deriving (Show, Eq, Binary, Generic)
 
-broadcastProcess :: ReceivePort GameState
-                 -> ReceivePort (SendPort GameState)
-                 -> Process ()
+broadcastProcess
+  :: (Show a, Serializable a)
+  => ReceivePort a -> ReceivePort (SendPort a) -> Process b
 broadcastProcess inboundGame subscriptionRequests = loop Set.empty
   where
     loop subscribers = do
@@ -148,17 +107,18 @@ broadcastProcess inboundGame subscriptionRequests = loop Set.empty
       mergedPorts <-
         mergePortsRR
           [NewState <$> inboundGame, Subscribe <$> subscriptionRequests]
-      msg :: BroadcasterMsg GameState <- receiveChan mergedPorts
+      msg <- receiveChan mergedPorts
       case msg of
         NewState state -> do
           liftIO . putStrLn $ "B: I should broadcast: " <> show state
           liftIO . putStrLn $ "B: ...to: " <> show subscribers
-          mapM_ (`sendChan` state) $ Set.toList subscribers
+          traverse_ (`sendChan` state) $ Set.toList subscribers
           loop subscribers
         Subscribe newSubscriber -> do
           liftIO . putStrLn $ "B: adding: " <> show newSubscriber
           loop (Set.insert newSubscriber subscribers)
 
+------------------------------------------------------------
 data GameMsg =
   GameMsg Text
   deriving (Show, Eq, Binary, Generic)
@@ -168,6 +128,14 @@ data GameState = GameState
   , msgCount :: Int
   } deriving (Show, Eq, Binary, Generic)
 
+initialGameState :: GameState
+initialGameState =
+  GameState
+  { lastMsg = Nothing
+  , msgCount = 0
+  }
+
+-- TODO Here we could split out a pure handler function.
 gameProcess :: ReceivePort GameMsg
             -> SendPort GameState
             -> GameState
